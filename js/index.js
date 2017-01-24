c4f79e313f6d20a20580447e82adb76d5d08e7c5
Sachin Kelkar <sachinkel19@gmail.com>
2017-01-24 22:41:53 +0530

    Done

--- js/index.js
+++ js/index.js
@@ -1,46 +1,69 @@
 window.addEventListener('load', init, false);
 
 var canvas, ctx, points, gravity;
-var width, height, mouse;
+var width, height;
+var mouse;
+// var mouse = {
+//   x: 0,
+//   y: 0,
+// };
+var vinit = 1;
+
+window.requestAnimationFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.oRequestAnimationFrame      ||
+          window.msRequestAnimationFrame     ||
+          function (callback) {
+              window.setTimeout(callback, 1000 / 60);
+          };
+})();
 
 var s = {
-  num_points: 1,
-  gravity: 10,
-  mousemass: 10,
+  num_points: 100,
+  mousemass: 150,
 }
 
+var gui = new dat.GUI();
+
+gui.add(s, "num_points").min(50).max(200).step(1).name("No. of points");
+gui.add(s, "mousemass").min(75).max(400).step(1).name("Mouse mass");
+gui.close();
+
 class Point {
-  constructor() {
-    this.vx = Math.random();
-    this.vy = Math.random();
-    this.px = Math.random();
-    this.py = Math.random();
-    this.mass = Math.random();
+  constructor(w, h) {
+    this.vx = Math.random()*2*vinit-vinit;
+    this.vy = Math.random()*2*vinit-vinit;
+    this.px = Math.floor(Math.random()*w);
+    this.py = Math.floor(Math.random()*h);
+    // this.mass = Math.random();
   }
 
-  update() {
-    var x = mouse.x;
-    var y = mouse.y;
+  update(mouse) {
+    if(mouse) {
+      var dx = mouse.x - this.px;
+      var dy = mouse.y - this.py;
 
-    var dx = mouse.x - x;
-    var dy = mouse.y - y;
+      var force = s.mousemass / Math.pow((dx*dx+s.mousemass/2+dy*dy+s.mousemass/2), 1.4);
 
-    var theta = Math.arctan(dy/dx);
+      var ax = force*dx;
+      var ay = force*dy;
 
-    // a = GM/r*r
-    var a = G*s.mousemass / (r*r);
-    var ax = a*Math.cos(theta);
-    var ay = a*Math.sin(theta);
+      this.vx += ax;
+      this.vy += ay;
+    }
 
-    // s = ut + 0.5at*t
-    this.px += this.vx*delta + 0.5*ax*delta*delta;
-    this.py += this.vy*delta + 0.5*ay*delta*delta;
+    this.px += this.vx;
+    this.py += this.vy;
 
-    // v = u + at
-    this.vx += ax*s.delta;
-    this.vy += ay*s.delta;
+    if(this.px < 0 || this.px > width) {
+      this.vx *= -1;
+    }
+    if(this.py < 0 || this.py > height) {
+      this.vy *= -1;
+    }
   }
-
 }
 
 function init() {
@@ -52,44 +75,85 @@ function init() {
   height = canvas.height = window.innerHeight;
 
   // Init mouse coordinates
-  mouse = {
-    x: width/2,
-    y: height/2,
-  };
+  // mouse = {
+  //   x: width/2,
+  //   y: height/2,
+  // };
 
   canvas.addEventListener("mousemove", getMousePos, false);
-  
-  // points = assignPoints(POINTS);
+  canvas.addEventListener("touchmove", getMousePos, false);
+  canvas.addEventListener("mousedown", function() {
+    s.mousemass *= s.mousemass > 0 ? -1 : 1;
+  }, false);
+  canvas.addEventListener("mouseup", function() {
+    s.mousemass *= s.mousemass < 0 ? -1 : 1;
+  }, false);
+  canvas.addEventListener("mouseleave", function(e) {
+    mouse = null;
+  }, false);
+
+
+  points = assignPoints(s.num_points);
   window.requestAnimationFrame(draw);
 }
 
 function draw() {
   var i;
-  // Transparency code
 
-  // // Add and delete points if necessary
-  // if(points.length < s.num_points) {
-  //   points = points.concat(assignPoints(s.num_points - points.length));
-  // } else if(points.length > s.num_points) {
-  //   points = removePoints(points, points.length - s.num_points);
-  // }
-  // // Update all points
-  // for(i = 0; i < points.length; i++) {
-  //   points[i].update();
-  // }
+  // Add and delete points if necessary
+  if(points.length < s.num_points) {
+    points = points.concat(assignPoints(s.num_points - points.length));
+  } else if(points.length > s.num_points) {
+    points = removePoints(points, s.num_points);
+  }
+  // Update all points
+  for(i = 0; i < points.length; i++) {
+    points[i].update(mouse);
+  }
+  // Transparency code
+  ctx.save();
+  ctx.beginPath();
+  ctx.fillStyle = 'rgba(0, 51, 51, 0.6)';
+  ctx.fillRect(0, 0, width, height);
+
+  if(mouse) {
+    var gradient = ctx.createRadialGradient(mouse.x, mouse.y, Math.abs(s.mousemass*0.75), mouse.x, mouse.y, Math.max(width, height));
+    gradient.addColorStop(0,"rgba(0, 0, 0, 0)");
+    gradient.addColorStop(1,"black");
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, width, height);
+  }
+
   // Draw all points
+  for(i = 0; i < points.length; i++) {
+    var pt = points[i];
+    ctx.moveTo(Math.round(pt.px), Math.round(pt.py));
+    ctx.arc(Math.round(pt.px), Math.round(pt.py), 1, 0, 2*Math.PI);
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.fill();
+  }
+  ctx.restore();
 
   window.requestAnimationFrame(draw);
 }
 
 function assignPoints(no_points) {
-  pts = [];
-
+  var i;
+  var a = [];
+  for(i = 0; i < no_points; i++) {
+    var pt = new Point(width, height);
+    a.push(pt);
+  }
+  return a;
 }
 
-function removePoints(points, num_points) {
-  var pts = points.slice();
-
+function removePoints(points, limit) {
+  var len = points.length;
+  while(len > limit) {
+    points.splice(Math.floor(Math.random()*len), 1);
+    len--;
+  }
+  return points;
 }
 function getMousePos(e) {
   var rect = canvas.getBoundingClientRect();
